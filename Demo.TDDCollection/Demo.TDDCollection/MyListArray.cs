679039fc605c6e601a5cfc2e31ea76f281afe8f8
Alexey <godhedin2000@gmail.com>
2019-02-11 13:11:07 +0200

    Demo.TDDCollecion added

---
+++ Demo.TDDCollection/Demo.TDDCollection/MyListArray.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.TDDCollection
+{
+    public class MyListArray<T> : IList<T>, IEnumerator<T>
+    {
+        #region private fields
+
+        private T[] _arr;
+        private int _count;
+
+        #endregion
+        
+        #region .ctors
+
+        public MyListArray(int startCapacity = 10)
+        {
+            _arr = new T[startCapacity];
+            _count = 0;
+        }
+
+        #endregion
+
+        #region indexer
+
+        public T this[int index] {
+            get
+            {
+                if (index < 0 || index >= _count)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                return _arr[index];
+            }
+            set {
+                if (index < 0 || index >= _count)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                _arr[index] = value;
+            }
+        }
+
+        #endregion
+
+        public int Count => _count;
+
+        public bool IsReadOnly => false;
+
+        public void Add(T val)
+        {
+            if (_count == _arr.Length)
+            {
+                Array.Resize(ref _arr, _count + 10);
+            }
+            _arr[_count++] = val;
+        }
+
+        public void Clear()
+        {
+            _count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region Enumerable
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+
+        #region Enumerator
+
+        int _currentIndex = -1;
+
+        public T Current => _arr[_currentIndex];
+        object IEnumerator.Current => Current;
+
+        public bool MoveNext()
+        {           
+            return ++_currentIndex < _count;
+        }
+        public void Reset()
+        {
+            _currentIndex = -1;
+        }
+        public void Dispose()
+        {
+            Reset();
+        }
+
+        #endregion;
+    }
+}
---
+++ Demo.TDDCollection/UnitTestProject/ArrayListUnitTest.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Demo.TDDCollection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class ListArrayUnitTest
+    {
+        [TestMethod]
+        public void Ctor_1_TestMethod()
+        {
+            MyListArray<int> list = new MyListArray<int>();
+            Assert.IsNotNull(list);
+            Assert.IsFalse(list.IsReadOnly);
+            Assert.AreEqual(0, list.Count);
+        }
+        [TestMethod]
+        public void Ctor_2_TestMethod()
+        {
+            MyListArray<int> list = new MyListArray<int>(100);
+            Assert.IsNotNull(list);
+            Assert.IsFalse(list.IsReadOnly);
+            Assert.AreEqual(0, list.Count);
+        }
+        [TestMethod]
+        public void AddAndCountTestMethod()
+        {
+            MyListArray<int> list = new MyListArray<int>(2);
+            Assert.AreEqual(0, list.Count);
+            list.Add(100);
+            Assert.AreEqual(1, list.Count);
+            list.Add(101);
+            Assert.AreEqual(2, list.Count);
+            list.Add(102);
+            Assert.AreEqual(3, list.Count);
+            list.Add(103);
+            Assert.AreEqual(4, list.Count);
+        }
+        [TestMethod]
+        public void IndexTestMethod()
+        {
+            MyListArray<int> list = new MyListArray<int>(2);            
+            list.Add(100);            
+            list.Add(101);
+            list.Add(102);            
+            list.Add(103);
+            Assert.AreEqual(4, list.Count);
+            Assert.AreEqual(100, list[0]);
+            Assert.AreEqual(101, list[1]);
+            Assert.AreEqual(102, list[2]);
+            Assert.AreEqual(103, list[3]);
+            list[0] = 0;
+            Assert.AreEqual(0, list[0]);
+            list[1] = 1;
+            Assert.AreEqual(1, list[1]);
+            list[2] = 2;
+            Assert.AreEqual(2, list[2]);
+            list[3] = 3;
+            Assert.AreEqual(3, list[3]);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void IndexException_get0_TestMethod()
+        {
+            MyListArray<int> list = new MyListArray<int>(2);            
+            int x = list[1];
+        }
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void IndexException_get1_TestMethod()
+        {
+            MyListArray<int> list = new MyListArray<int>(2);
+            int x = list[-1];
+        }
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void IndexException_set0_TestMethod()
+        {
+            MyListArray<int> list = new MyListArray<int>(2);
+            list[1]=1;
+        }
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void IndexException_set1_TestMethod()
+        {
+            MyListArray<int> list = new MyListArray<int>(2);
+            list[-1]=10;
+        }
+
+        [TestMethod]
+        public void ClearTestMethod()
+        {
+            MyListArray<int> list = new MyListArray<int>(2);
+            list.Add(100);
+            list.Add(101);
+            list.Add(102);
+            list.Add(103);
+            Assert.AreEqual(4, list.Count);
+            list.Clear();
+            Assert.AreEqual(0, list.Count);
+        }
+        [TestMethod]
+        public void IEnumerableTestMethod()
+        {
+            MyListArray<int> list = new MyListArray<int>(100);
+            var e = list.GetEnumerator();
+            Assert.IsFalse(e.MoveNext());
+            e.Reset();
+            int[] mas = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            foreach(int val in mas)
+            {
+                list.Add(val);
+            }
+            int count = 0;
+            using (IEnumerator<int> le = list.GetEnumerator())
+            {
+                while (le.MoveNext())
+                    Assert.AreEqual(le.Current, mas[count++]);
+            }
+        }
+    }
+}
